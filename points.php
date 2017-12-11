8ef257cca028e3ef016438913b0d887fb0c31018
Oliver Czempas <oliver@czempas.de>
2017-12-11 14:37:07 +0100

    Update

--- points.php
+++ points.php
@@ -4,7 +4,7 @@ $pdo = new PDO('mysql:host=localhost;dbname=inseccam', 'inseccam', 'yaq1wsx_');
 $sql = "SELECT * FROM streams";
 $rows = array();
 foreach ($pdo->query($sql) as $row) {
-    $rows[] = $r;
+    $rows[] = $row;
 }
 print json_encode($rows);
 ?>
