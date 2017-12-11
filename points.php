29d8a5f4c9b9bc625986c47723ddb52147dbd68d
Oliver Czempas <oliver@czempas.de>
2017-12-11 14:35:23 +0100

    Print JSON

--- points.php
+++ points.php
@@ -2,9 +2,9 @@
 
 $pdo = new PDO('mysql:host=localhost;dbname=inseccam', 'inseccam', 'yaq1wsx_');
 $sql = "SELECT * FROM streams";
+$rows = array();
 foreach ($pdo->query($sql) as $row) {
-   echo $row['city']." ".$row['country']."<br />";
-   echo "Link: ".$row['source']."<br /><br />";
+    $rows[] = $r;
 }
-
+print json_encode($rows);
 ?>
