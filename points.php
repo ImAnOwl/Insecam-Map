ef89f6bc283c452fc1b51f143660c88f05abe6a3
Oliver Czempas <oliver@czempas.de>
2017-12-11 14:40:07 +0100

    JSON GET

--- points.php
+++ points.php
@@ -3,8 +3,20 @@
 $pdo = new PDO('mysql:host=localhost;dbname=inseccam', 'inseccam', 'yaq1wsx_');
 $sql = "SELECT * FROM streams";
 $rows = array();
+
+if(isset($_GET['json'])){
+
 foreach ($pdo->query($sql) as $row) {
     $rows[] = $row;
 }
 print json_encode($rows);
+
+} else {
+
+    foreach ($pdo->query($sql) as $row) {
+        echo $row["country"] . " - " . $row["city"];
+        echo $row["source"];
+    }
+
+}
 ?>
